68933b4d1b8593b1748dded344c1b741c7bd3261
Kyutae Park <zldrbxo1078@naver.com>
2022-02-28 11:29:20 +0900

    초기 커밋.

---
+++ Ex01_basic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_basic
+{
+    class Car
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello world");
+            char c; //2byte 0~255
+            c = 'A';
+
+            char d;
+            d = '나';
+            Console.WriteLine("c:{0}, d:{1}", c,d);
+
+            //지역변수는 반드시 [초기화]를 통해서 사용한다.
+            short s = 10000;
+
+        }
+    }
+}
---
+++ Ex03_EmpLib/Emp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03_EmpLib
+{
+    public class Emp    //설계도
+    {
+        //속성(정보를 저장) + 기능
+        private int empno;
+        private string ename;
+        private int sal;
+
+        public Emp() { }
+        public Emp(int empno, string ename, int sal)
+        {
+            this.empno = empno;
+            this.ename = ename;
+            this.sal = sal;
+        }
+        //private >> 캡슐화
+        public int Empno
+        {
+            get { return empno; }
+            set { empno = value; }
+        }
+        public string Ename
+        {
+            get { return ename; }
+            set { ename = value; }
+        }
+        public int Sal
+        {
+            get { return sal; }
+            set { sal = value; }
+        }
+
+        public void EmpPrint()
+        {
+            Console.WriteLine("{0} - {1} - {2}", this.empno, this.ename, this.sal);
+        }
+    }
+}
