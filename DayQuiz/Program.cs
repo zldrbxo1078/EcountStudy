4652a2b4eda4b58a11d454d76955840200780f1a
Kyutae Park <zldrbxo1078@naver.com>
2022-03-04 17:59:05 +0900

    2022.03.04 강의

---
+++ DayQuiz/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DayQuiz
+{
+    
+    class Seat
+    {
+        string[,] seat = new string[3, 5];
+        int select = 0;
+
+        public void SetSeat()
+        {
+
+            for (int i = 0; i < seat.GetLength(0); i++)
+            {
+                for (int j = 0; j < seat.GetLength(1); j++)
+                {
+
+                    seat[i, j] = "__";
+
+                }
+            }
+        }
+
+        public void ShowSeat()
+        {
+            for (int i = 0; i < seat.GetLength(0); i++)
+            {
+                for (int j = 0; j < seat.GetLength(1); j++)
+                {
+
+                    Console.Write(seat[i, j] == "__" ? $"[{i+1}-{j+1}]" : "[예매]");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Reservation(int row, int col, string customerNum)
+        {
+            if(seat[row - 1, col - 1] == "__")
+            {
+                seat[row - 1, col - 1] = customerNum;
+                Console.WriteLine("예매가 완료되었습니다.");
+                Console.WriteLine($"예매된 좌석 : {row}-{col} / 예매번호 : {customerNum}");
+            }
+            else if (seat[row - 1, col - 1] == "예매")
+            {
+                Console.WriteLine("이미 예약된 좌석입니다.");
+            }
+            else
+            {
+                Console.WriteLine("잘못된 입력입니다.");
+            }
+
+        }
+        public void ReserveCancel(string inputnum)
+        {
+            for (int i = 0; i < seat.GetLength(0); i++)
+            {
+                for (int j = 0; j < seat.GetLength(1); j++)
+                {
+                    if (seat[i, j] == inputnum)
+                    {
+                        Console.WriteLine($"고객님의 좌석은 {i+1}-{j+1}입니다.");
+                        Console.WriteLine("취소하시겠습니까?(예.1 아니오.2)");
+                        select = Int32.Parse(Console.ReadLine());
+                        if (select == 1)
+                        {
+                            seat[i, j] = "__";
+                            Console.WriteLine("예매가 취소되었습니다.");
+                            break;
+                        }
+                        break;
+                    }
+
+                }
+            }
+            
+        }
+        
+    }
+
+
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Seat seats = new Seat();
+            seats.SetSeat();
+            int row, col;
+            string customernum = (new Random()).Next().ToString();
+
+            string inputnum = "";
+
+            while (true)
+            {
+                Console.WriteLine("************************");
+                Console.WriteLine("*****영화 예매 시스템*****");
+                Console.WriteLine("************************");
+                Console.WriteLine("1. 예매하기\n");
+                Console.WriteLine("2. 예매조회\n");
+                Console.WriteLine("3. 예매취소\n");
+                Console.WriteLine("4. 종료\n");
+                int select = Int32.Parse(Console.ReadLine());
+
+                switch (select)
+                {
+                    case 1:
+                        Console.WriteLine("좌석의 행을 입력하세요");
+                        row = Int32.Parse(Console.ReadLine());
+                        Console.WriteLine("좌석의 열을 입력하세요");
+                        col = Int32.Parse(Console.ReadLine());
+
+                        seats.Reservation(row,col, customernum);
+                        break;
+                    case 2:
+                        seats.ShowSeat();
+                        break;
+                    case 3:
+                        Console.WriteLine();
+                        Console.WriteLine("예약번호를 입력해주세요");
+                        inputnum = Console.ReadLine();
+                        seats.ReserveCancel(inputnum);
+                        break;
+                    case 4:
+                        return;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Ex24_Array_Param/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex24_Array_Param
+{
+    //30line 이전
+
+    class paramArray
+    {
+        public int[] CopyArray(int[] source)
+        {
+            int[] target = new int[source.Length];
+            for (int i = 0; i < source.Length; i++)
+            {
+                target[i] = source[i] + 1;
+            }
+            return target;
+        }
+        public int[] CopyArray2(int[] source, int[] source2)
+        {
+            int[] ta = new int[source2.Length];
+            for (int i = 0; i < ta.Length; i++)
+            {
+                if (ta.Length == source2[i])
+                {
+                    ta[i] = source2[i];
+                }
+                else
+                {
+                    ta[i] = source[i];
+                }
+            }
+            return ta;
+        }
+    }
+   class Param
+    {
+        public void sample(params int[] arr)    //params 사용하면 배열의 주소를 받지 않아도 된다.
+        {                                       //나열된 값을 parameter 전달하면 인정 
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            paramArray pa = new paramArray();
+            int[] p = new int[] { 100, 200, 300, 400, 500 };
+            int[] so = pa.CopyArray(p); //주소값 전달
+
+            foreach (int item in so)
+            {
+                Console.WriteLine("so Array : {0}", item);
+            }
+
+            int[] p2 = new int[] { 1, 2, 3, 4, 5 };
+            int[] p3 = new int[] { 6, 7, 8, 9, 10 };
+
+            Param p4 = new Param();
+            p4.sample(10,20,30,40);
+        }
+    }
+}
