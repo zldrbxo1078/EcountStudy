1c50e2a1794ec9506e9befb218e7c2aa178050e2
kyutaePark <zldrbxo1078@naver.com>
2022-03-15 15:06:01 +0900

    quiz 커밋

--- hw_multipulication/Program.cs
+++ hw_multipulication/Program.cs
@@ -6,46 +6,49 @@ using System.Threading.Tasks;
 
 namespace hw_multipulication
 {
-    //class Result {
-    //    public string Query { get; }
-    //    public string Answer { get; set; }
-    //    public string Correct { get; set; }
-    //    public Result(string answer, Quizinfo quiz) {
-    //        this.Answer = answer;
-    //    }
-    //    public override string ToString()
-    //    {
-    //        return "문제 : "+ Query+"쓴 답 : " + Answer + "정답 : " + Correct;
-    //    }
-
-    //}
+    
     class Quizinfo
     {
-        
-        public Dictionary<string, string> Quizzes { get; }
-        public Quizinfo()
+        private string Question { get; }
+        private string Answer { get; }
+        private char OX { get; }
+        public Quizinfo(string question, string answer)
         {
-            Quizzes = new Dictionary<string, string>();
-        }
-
-
-        public void put(string quiz, string correctanswer)
-        {
-            this.Quizzes.Add(quiz, correctanswer);
+            this.Answer = answer;
+            this.Question = question;
         }
 
     }
+
+    class QuizGame
+    {
+        private List<Quizinfo> Quizzes;
+        private Dictionary<Quizinfo, int> Answers { get; }
+
+        public QuizGame()
+        {
+            Quizzes = new List<Quizinfo>();
+            Answers = new Dictionary<Quizinfo, int>();
+        }
+
+        public void putQuiz(string question, string answer)
+        {
+            this.Quizzes.Add(new Quizinfo(question, answer));
+        }
+    }
+
+
 
     class Program
     {
         static void Main(string[] args)
         {
             
-            Quizinfo quizinfo = new Quizinfo();
-            quizinfo.put("1+1", "2");
-            quizinfo.put("2+2", "4");
-            quizinfo.put("3+3", "6");
-            quizinfo.put("4+4", "8");
+            QuizGame quizgame = new QuizGame();
+            quizgame.putQuiz("1+1", "2");
+            quizgame.putQuiz("2+2", "4");
+            quizgame.putQuiz("3+3", "6");
+            quizgame.putQuiz("4+4", "8");
 
             foreach (var item in quizinfo.Quizzes)
             {
