3d05dbce55e89f4f75e24055792ae5e29c723460
Kyutae Park <zldrbxo1078@naver.com>
2022-03-16 11:52:31 +0900

    0316

---
+++ Ex05_FileStream/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05_FileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //fileStream을 통해 text 형태의 파일을 접근할 수 있다. 
+            /*
+            FileStream fs = null;
+            StreamWriter streamWriter = null;
+            try
+            {
+                fs = new FileStream("hello.txt", FileMode.OpenOrCreate);
+                streamWriter = new StreamWriter(fs);
+
+                int data = 100;
+                float fdata = 3.14f;
+                string strdata = "hello world";
+
+                streamWriter.Write(data);
+                streamWriter.Write(fdata);
+                streamWriter.Write(strdata);
+
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                streamWriter.Close();
+                fs.Close();
+            }
+            */
+            using(StreamWriter sw = new StreamWriter(new FileStream("hello3.txt", FileMode.OpenOrCreate))){
+                int data = 100;
+                float fdata = 3.14f;
+                string strdata = "hello world";
+
+                sw.WriteLine(data);
+                sw.WriteLine(fdata);
+                sw.WriteLine(strdata);
+            }   //자동 close() = finally
+        }
+    }
+}
---
+++ Ex06_FileStream/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex06_FileStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            FileStream fs = new FileStream("hello.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            int data = int.Parse(sr.ReadLine());
+            float fdata = float.Parse(sr.ReadLine());
+            string strdata = sr.ReadLine();
+            sr.Close();
+            fs.Close();
+            Console.WriteLine($"{data} - {fdata} - {strdata}");
+            */
+            using (StreamReader sr = new StreamReader(new FileStream("hello3.txt", FileMode.Open, FileAccess.Read)))
+            {
+                int data = int.Parse(sr.ReadLine());
+                float fdata = float.Parse(sr.ReadLine());
+                string strdata = sr.ReadLine();
+
+                Console.WriteLine($"{data} - {fdata} - {strdata}");
+
+            }   //자동 close() = finally
+        }
+    }
+}
